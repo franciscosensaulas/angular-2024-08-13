3706bdbd0b759b58c8b7dc88571987452c6c581e
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2024-08-14 22:14:31 -0300

    Curso Angular Aula 02

--- src/app/produto-lista/produto-lista.component.ts
+++ src/app/produto-lista/produto-lista.component.ts
@@ -10,28 +10,58 @@ import { FormsModule } from '@angular/forms';
 })
 export class ProdutoListaComponent {
   nomeProduto: string = '';
+  indiceAlterar: number = -1;
+  mensagemErro: string = '';
+  tituloBotaoSalvarProduto: string = 'Cadastrar'
 
   produtos: Array<string> = [
-    // "iPhone 14",
-    // "Motorola G7",
-    // "Xioami 9",
-    // "Nokia"
+    "iPhone 14",
+    "Motorola G7",
+    "Xioami 9",
+    "Nokia"
   ]
 
-  cadastrarProduto() {
+  salvarProduto() {
+    // Limpar mensagem de erro
+    this.mensagemErro = '';
+
+    if (this.nomeProduto.length < 3){
+      this.mensagemErro = "Produto deve conter no mínimo 3 caracteres";
+      return;
+    }
+
     // Verificar se o produto está cadastrado
     let existeProduto = this.produtos.some(x => x === this.nomeProduto);
-    if (existeProduto === true)
+    if (existeProduto === true) {
+      this.mensagemErro = `Produto já cadastrado com o nome '${this.nomeProduto}'`;
       return;
+    }
 
-    // Adicionar o nome do produto que o usuário preencheu no input na lista de produtos
-    this.produtos.push(this.nomeProduto);
+    // Verificar se está no modo de cadastro
+    if (this.indiceAlterar === -1) {
+      // Adicionar o nome do produto que o usuário preencheu no input na lista de produtos
+      this.produtos.push(this.nomeProduto);
+    } else {
+      // Alterar o nome do produto no indice que o usuário escolheu para editar
+      this.produtos[this.indiceAlterar] = this.nomeProduto;
+      // Reset para o usuário poder cadastrar novamente
+      this.indiceAlterar = -1;
+      // Redefinir o texto do botão para Cadastrar, para que o usuário saiba que está Cadastrando um produto
+      this.tituloBotaoSalvarProduto = "Cadastrar";
+    }
     // Limpar campo
     this.nomeProduto = "";
   }
 
-  apagarProduto(nomeProduto: string){
+  apagarProduto(nomeProduto: string) {
     let indiceProduto = this.produtos.findIndex(x => x === nomeProduto);
     this.produtos.splice(indiceProduto, 1);
+  }
+
+  // Preencher os campos com os dados do produto
+  editarProduto(nomeProduto: string) {
+    this.indiceAlterar = this.produtos.findIndex(x => x === nomeProduto);
+    this.tituloBotaoSalvarProduto = "Editar";
+    this.nomeProduto = nomeProduto;
   }
 }
