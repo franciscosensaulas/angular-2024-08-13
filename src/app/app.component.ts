a70fc817ae7e95dde3ed1e49ca063078876a21e8
Francisco Lucas Sens <francisco.lucas.sens@gmail.com>
2024-08-15 22:05:57 -0300

    Curso Angular Aula 03

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,21 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { FormsModule } from '@angular/forms';
 import { ProdutoListaComponent } from './produto-lista/produto-lista.component';
+import { FilmeListaComponent } from './filme-lista/filme-lista.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FormsModule, ProdutoListaComponent],
+  imports: [
+    RouterLink, // Diretiva usada para criar links que permitem a navegação entre componentes
+    RouterLinkActive, // Diretiva usada para aplicar classes CSS dinamicamente em um link
+    RouterOutlet, // Componente Angular que atua como um componente de
+    //  ancoragem ou contâiner onde os componentes serão renderizados
+    FormsModule,
+    ProdutoListaComponent,
+    FilmeListaComponent,
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { ProdutoListaComponent } from './produto-lista/produto-lista.component';
+import { FilmeListaComponent } from './filme-lista/filme-lista.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { "path": "produtos", component: ProdutoListaComponent },
+    { "path": "filmes", component: FilmeListaComponent },
+];
